66e7e5ff52e146273b5ed097c1fced1d848ca0af
Charles Bozarth <amiacs@outlook.com>
2018-03-23 18:58:06 -0400

    Add Arduino code to cycle trackball LED color. Add drafts folder.

---
+++ code/Trackball_RGB_LED/Trackball_RGB_LED.ino
@@ -0,0 +1,107 @@
+// Note that brown wire is for 5V.
+
+// Define Pins
+#define RED 3
+#define GREEN 5
+#define BLUE 6
+
+#define delayTime 10 // fading time between colors
+
+
+void setup()
+{
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+}
+
+// define variables
+int redValue;
+int greenValue;
+int blueValue;
+
+
+// main loop
+void loop()
+{
+  cycleSingleColor(GREEN);
+  cycleSingleColor(BLUE);
+  turnOnSingleColor(RED);
+  delay(1000);
+
+  redValue = 255; // choose a value between 1 and 255 to change the color.
+  greenValue = 0;
+  blueValue = 0;
+  for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
+  {
+    redValue -= 1;
+    greenValue += 1;
+    analogWrite(RED, 255 - redValue);
+    analogWrite(GREEN, 255 - greenValue);
+    delay(delayTime * 5);
+  }
+
+  redValue = 0;
+  greenValue = 255;
+  blueValue = 0;
+  for(int i = 0; i < 255; i += 1)  // fades out green bring blue full when i=255
+  {
+    greenValue -= 1;
+    blueValue += 1;
+    analogWrite(GREEN, 255 - greenValue);
+    analogWrite(BLUE, 255 - blueValue);
+    delay(delayTime * 5);
+  }
+
+  redValue = 0;
+  greenValue = 0;
+  blueValue = 255;
+  for(int i = 0; i < 255; i += 1)  // fades out blue bring red full when i=255
+  {
+  redValue += 1;
+  blueValue -= 1;
+  analogWrite(RED, 255 - redValue);
+  analogWrite(BLUE, 255 - blueValue);
+  delay(delayTime * 5);
+  }
+
+  turnOffSingleColor(RED);
+}
+
+
+void cycleSingleColor(int pin)
+{
+  for(int i = 0; i < 255; i += 1)
+  {
+    analogWrite(pin, 255 - i);
+    delay(delayTime);
+  }
+
+  for(int i = 255; i >= 0; i -= 1)
+  {
+    analogWrite(pin, 255 - i);
+    delay(delayTime);
+  }
+}
+
+void turnOnSingleColor(int pin)
+{
+  for(int i = 0; i < 255; i += 1)
+  {
+    analogWrite(pin, 255 - i);
+    delay(delayTime);
+  }
+}
+
+void turnOffSingleColor(int pin)
+{
+  for(int i = 255; i >= 0; i -= 1)
+  {
+    analogWrite(pin, 255 - i);
+    delay(delayTime);
+  }
+}
+
---
+++ docs/_drafts/parts-list.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title:  "Parts List"
+date:   2018-02-25 12:00:00
+---
+|Part|Link|Cost|
+|---|---|---|
+|Electric ICE-T™ Deluxe RGB Arcade Trackball V2.5|http://groovygamegear.com/webstore/index.php?main_page=product_info&cPath=74&products_id=270|$116.95|
+|Trackball Mounting Kit - 3"|http://groovygamegear.com/webstore/index.php?main_page=product_info&cPath=74&products_id=314|$13.95|
+|CanaKit Raspberry Pi 3 with 2.5A Micro USB Power Supply|http://a.co/fu1tPRN|$42.99|
+|Micro SD Card 32 GB||$13.00|
+|Acer R240HY bidx 23.8-Inch IPS Monitor|http://a.co/5v14bSI|$129.99|
