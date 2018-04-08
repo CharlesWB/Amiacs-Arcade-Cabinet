071b677e577865c700dd6e335ad6342475f545ca
Charles Bozarth <amiacs@outlook.com>
2018-04-08 12:59:37 -0400

    Add Arduino test code for light strip controller.

---
+++ code/Test_P9813_Light_Strip_Controller/Test_P9813_Light_Strip_Controller.ino
@@ -0,0 +1,18 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 1
+
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void loop() {
+  static uint8_t hue = 0;
+  FastLED.showColor(CHSV(hue++, 255, 255)); 
+  delay(200);
+}
