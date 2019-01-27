dd6234c2b5e5e6164bc09cf67fdb8a3fc4b1b9c0
Charles Bozarth <amiacs@outlook.com>
2019-01-27 18:08:07 -0500

    Add test code for Raspberry Pi to Arduino I2C communication.

---
+++ code/Test_Pi_Arduino_i2c/Test_Pi_Arduino_i2c.ino
@@ -0,0 +1,56 @@
+// Testing the communication between Raspberry Pi and Arduino using I2C.
+
+// https://oscarliang.com/raspberry-pi-arduino-connected-i2c/
+// https://www.arduino.cc/en/Tutorial/MasterWriter
+
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+
+void setup() {
+  pinMode(13, OUTPUT);
+
+  Serial.begin(9600);
+
+  Wire.begin(SLAVE_ADDRESS);
+
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready!");
+}
+
+void loop() {
+  delay(100);
+}
+
+void receiveData(int byteCount){
+  Serial.print("byte count: ");
+  Serial.println(byteCount);
+
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
+
+    if (number == 1){
+      if (state == 0){
+        digitalWrite(13, HIGH);
+        Serial.println("LED on");
+        state = 1;
+      }
+      else{
+        digitalWrite(13, LOW);
+        Serial.println("LED off");
+        state = 0;
+      }
+    }
+  }
+}
+
+void sendData(){
+  Wire.write(number);
+}
+
