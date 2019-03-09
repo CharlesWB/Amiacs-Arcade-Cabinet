691cebbb5a0f0083df07cf6121dd6fb84c5eeaf5
Charles Bozarth <amiacs@outlook.com>
2019-03-09 10:26:43 -0500

    Implement a FastLED controller for the trackball tri-color LEDs.

--- code/Amiacs_Light_Controller/Amiacs_Light_Controller.ino
+++ code/Amiacs_Light_Controller/Amiacs_Light_Controller.ino
@@ -1,21 +1,40 @@
-
 /*
 Ideas
 - Would a sine wave, breathe, heartbeat, or some kind of bounce effect for the cycle time make it interesting?
+- displayModes may turn into the command byte of I2C. It would define the action to be done. Such as set or enable.
 */
 
+#include "FastLED.h"
+#include "SingleTriColorLEDController.h"
+
+CRGB defaultSystemColor = CRGB::Orange;
+CRGB playerColors[2] = {CRGB::Blue, CRGB::Red};
+
+#define TRACKBALL_RED_PIN 3
+#define TRACKBALL_GREEN_PIN 5
+#define TRACKBALL_BLUE_PIN 6
+#define NUM_TRACKBALLS 1
+CRGB trackballs[NUM_TRACKBALLS];
+
+
 enum displayModes {
-    ATTRACT,
-    MUTE, // Or DIMMED, SCREENSAVER_ACTIVE
-    GAME_PLAYING,
-    GAME_PAUSED, // RetroPie may not provide a way to respond to paused games.
-    TESTING,
+  ATTRACT,
+  MUTE, // Or DIMMED, SCREENSAVER_ACTIVE
+  GAME_PLAYING,
+  GAME_PAUSED, // RetroPie may not provide a way to respond to paused games.
+  TESTING,
 };
 
-void setup() {
 
+void setup() {
+  SingleTriColorLEDController<TRACKBALL_RED_PIN, TRACKBALL_GREEN_PIN, TRACKBALL_BLUE_PIN> trackballLEDController;
+  FastLED.addLeds(&trackballLEDController, trackballs, NUM_TRACKBALLS).setCorrection(TypicalLEDStrip);
+  trackballs[0] = defaultSystemColor;
+
+  FastLED.show();
 }
 
 void loop() {
 
 }
+
---
+++ code/Amiacs_Light_Controller/SingleTriColorLEDController.h
@@ -0,0 +1,38 @@
+#ifndef __INC_SINGLETRICOLORLEDCONTROLLER_H
+#define __INC_SINGLETRICOLORLEDCONTROLLER_H
+
+#include "FastLED.h"
+
+/// Single tri-color (4 pin) LED controller class
+/// @tparam RED_PIN the pin to write data out for the red LED
+/// @tparam GREEN_PIN the pin to write data out for the green LED
+/// @tparam BLUE_PIN the pin to write data out for the blue LED
+/// @tparam RGB_ORDER the RGB ordering for the led data
+template<uint8_t RED_PIN, uint8_t GREEN_PIN, uint8_t BLUE_PIN, EOrder RGB_ORDER = RGB>
+class SingleTriColorLEDController : public CPixelLEDController<RGB_ORDER> {
+public:
+  SingleTriColorLEDController() {}
+
+protected:
+  virtual void init() {
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
+
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+  }
+
+  virtual void showPixels(PixelController<RGB_ORDER> &pixels) {
+      uint8_t r = pixels.loadAndScale0();
+      uint8_t g = pixels.loadAndScale1();
+      uint8_t b = pixels.loadAndScale2();
+
+      analogWrite(RED_PIN, r);
+      analogWrite(GREEN_PIN, g);
+      analogWrite(BLUE_PIN, b);
+  }
+};
+
+#endif
