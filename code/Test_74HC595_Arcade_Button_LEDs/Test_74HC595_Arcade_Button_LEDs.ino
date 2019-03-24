e0ada4f498e07edaeeb07af0a5d58f5ae4738860
Charles Bozarth <amiacs@outlook.com>
2019-03-24 16:00:07 -0400

    Add test of arcade button LEDs using 74HC595 as an alternative to I2C and MCP23017.

---
+++ code/Test_74HC595_Arcade_Button_LEDs/Test_74HC595_Arcade_Button_LEDs.ino
@@ -0,0 +1,40 @@
+/*
+Has three 74HC595's for 24 bits (i.e. buttons).
+
+References
+https://www.arduino.cc/en/tutorial/ShiftOut
+https://shiftregister.simsso.de/
+*/
+
+#include <ShiftRegister74HC595.h>
+
+int numberOfShiftRegisters = 3;
+
+//Pin connected to latch pin (ST_CP) of 74HC595
+const int latchPin = 10;
+//Pin connected to clock pin (SH_CP) of 74HC595
+const int clockPin = 13;
+////Pin connected to Data in (DS) of 74HC595
+const int dataPin = 11;
+
+ShiftRegister74HC595 sr (numberOfShiftRegisters, dataPin, clockPin, latchPin);
+
+int pin = 0;
+
+void setup() {
+  sr.setAllLow();
+}
+
+void loop() {
+  for(int pin = 0; pin < 24; pin++) {
+    if(pin == 0) {
+      sr.set(23, LOW);
+    }
+    else if(pin > 0) {
+      sr.set(pin - 1, LOW);
+    }
+    sr.set(pin, HIGH);
+    delay(250);
+  }
+}
+
