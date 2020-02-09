dd42a2af0cd80313d8ac1250ea4accd19b8d49bf
Charles Bozarth <amiacs@outlook.com>
2020-02-09 15:13:34 -0500

    Add Centipede game lights.

--- code/Amiacs-Event-Processor/Amiacs-Event-Processor.py
+++ code/Amiacs-Event-Processor/Amiacs-Event-Processor.py
@@ -243,7 +243,9 @@ systemLights = {
 
 gameLights = {
     'asteroid.zip': CabinetLights(
-        PlayerLights(B=Light.On, A=Light.On, Y=Light.On, Start=Light.On, Select=Light.On, HotKey=Light.On, Command=Light.On), False)
+        PlayerLights(B=Light.On, A=Light.On, Y=Light.On, Start=Light.On, Select=Light.On, HotKey=Light.On, Command=Light.On), False),
+    'centiped.zip': CabinetLights(
+        PlayerLights(B=Light.On, Start=Light.On, Select=Light.On, HotKey=Light.On, Command=Light.On), False)
 }
 
 logging.basicConfig(
