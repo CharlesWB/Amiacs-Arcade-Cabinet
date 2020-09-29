350f6199c172c6bdb174febb3effbab39ee2ae52
Charles Bozarth <amiacs@outlook.com>
2020-09-29 16:35:44 -0400

    Test P9813 with dual LED controllers.

--- code/Test_P9813_Light_Strip_Controller/Test_P9813_Light_Strip_Controller.ino
+++ code/Test_P9813_Light_Strip_Controller/Test_P9813_Light_Strip_Controller.ino
@@ -1,18 +1,41 @@
 #include "FastLED.h"
 
-#define NUM_LEDS 1
+#define NUM_LEDS 2
 
-#define DATA_PIN 3
+#define DATA_PIN 11
 #define CLOCK_PIN 13
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
+
+  leds[0] = CRGB::Blue;
+  leds[1] = CRGB::Red;
+
+  FastLED.show();
+
+  delay(2000);
+
+  leds[0] = CRGB::Red;
+  leds[1] = CRGB::Green;
+
+  FastLED.show();
+
+  delay(2000);
+
+  leds[0] = CRGB::Green;
+  leds[1] = CRGB::Blue;
+
+  FastLED.show();
+
+  delay(2000);
 }
 
 void loop() {
+  
   static uint8_t hue = 0;
   FastLED.showColor(CHSV(hue++, 255, 255)); 
+
   delay(200);
 }
