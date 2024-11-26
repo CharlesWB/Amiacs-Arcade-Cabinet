4c2e2b2459998fa768d4737af12e15bbdcbfdd86
Charles Bozarth <charlesbozarth@outlook.com>
2024-11-26 11:41:56 -0500

    Update for compatibility with FastLED v3.9.x.

--- code/Amiacs_Light_Controller/Amiacs_Light_Controller.ino
+++ code/Amiacs_Light_Controller/Amiacs_Light_Controller.ino
@@ -601,8 +601,8 @@ void CycleTrackballBetweenPlayerColors() {
   if(millis() - startTime > 40) {
     fill_solid(trackballs, NUM_TRACKBALLS, ColorFromPalette(playerColorPalette, playerColorPaletteIndex));
 
-    // TODO Remove the hard-coding of the trackball LEDs index. Or resolve flicker when updating player lights.
-    FastLED[1].showLeds();
+    // TODO Remove the hard-coding of the trackball LEDs index. Or resolve flicker when updating player lights using FastLED.show().
+    FastLED[1].showLedsInternal(GetBrightness());
 
     playerColorPaletteIndex++;
 
